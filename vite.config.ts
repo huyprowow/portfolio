cdb72b630530950f6b40e64f6a66758edcb95a9b
Huy Bui <huyprowow@gmail.com>
2025-08-24 20:00:16 +0700

    file load issue

--- vite.config.ts
+++ vite.config.ts
@@ -41,8 +41,6 @@ export default defineConfig({
           'three-stdlib': 'THREE_STDLIB',
         },
         assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.')
-          const ext = info[info.length - 1]
           if (/\.(glb|fbx|pmx)$/.test(assetInfo.name)) {
             return `assets/[name].[ext]`
           }
