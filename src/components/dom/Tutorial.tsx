279727bc78c8600699c2ed7c30941cb2e51b72f8
Huy Bui <huyprowow@gmail.com>
2025-08-24 19:15:44 +0700

    resolution module

--- src/components/dom/Tutorial.tsx
+++ src/components/dom/Tutorial.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react'
-import { Live2DCubismFramework as live2dcubismframework } from '@/lib/CubismSdkForWeb-5-r.4/Framework/src/live2dcubismframework'
-import CubismFramework = live2dcubismframework.CubismFramework
+// import { Live2DCubismFramework as live2dcubismframework } from '@/lib/CubismSdkForWeb-5-r.4/Framework/src/live2dcubismframework'
+// import CubismFramework = live2dcubismframework.CubismFramework
 const Tutorial = () => {
-  useEffect(() => {
-    CubismFramework.startUp()
-  }, [])
+  // useEffect(() => {
+  //   CubismFramework.startUp()
+  // }, [])
   return <></>
 }
 
